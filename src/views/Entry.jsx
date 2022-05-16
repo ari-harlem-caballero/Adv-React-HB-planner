25008c445e625a61e79623bbf6865e492cb023c0
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-16 15:22:35 -0700

    refactor Entry for updateEntry.

--- src/views/Entry.jsx
+++ src/views/Entry.jsx
@@ -9,21 +9,52 @@ export default function Entry() {
   const { id } = useParams();
   const [entry, setEntry] = useState({});
   const { entries, getEntry } = useEntries();
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     setEntry(getEntry(id));
   }, [id, entries.length]);
 
-  return (
-    <>
-      <Link to="/entries" className={styles.backButton}>
-        &laquo; Back to Planner
-      </Link>
-      <article className={styles.entry}>
-        <h1>{entry?.title}</h1>
-        <p>Due: {entry?.date}</p>
-        <p>{entry?.content}</p>
-      </article>
-    </>
-  );
+  let content;
+  if (!isEditing) {
+    content = (
+      <>
+        <Link to="/entries" className={styles.backButton}>
+          &laquo; Back to Planner
+        </Link>
+        <article className={styles.entry}>
+          <h1>{entry?.title}</h1>
+          <p>Due: {entry?.date}</p>
+          <p>{entry?.content}</p>
+        </article>
+        <button type="button" onClick={() => setIsEditing(true)}>
+          Edit
+        </button>
+      </>
+    );
+  } else {
+    content = (
+      <>
+        <form>
+          <input
+            type="text"
+            value={entry.title}
+            onChange={(e) => {
+              onUpdate({ ...entry, title: e.target.value });
+            }}
+          />
+          <input
+            type="text"
+            value={entry.content}
+            onChange={(e) => {
+              onUpdate({ ...entry, content: e.target.value });
+            }}
+          />
+          <button>Update</button>
+        </form>
+      </>
+    );
+  }
+
+  return content;
 }
